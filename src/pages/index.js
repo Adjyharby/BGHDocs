68c89a1510da88d886a8293baffaa91d2aad6509
Adjourner_Harbringe <theenderwolfadjourner@gmail.com>
2026-02-10 13:12:54 +0800

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import clsx from 'clsx';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        header stuffs here
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={siteConfig.title}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        Main stuffs here
+      </main>
+    </Layout>
+  );
+}
